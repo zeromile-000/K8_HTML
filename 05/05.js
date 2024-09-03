493f797b3918298cf5c6b4ad631b211346b1fdf9
Junyoung_Kim <zzz5686@naver.com>
2024-09-03 18:04:07 +0900

    커밋이용

--- 05/05.js
+++ 05/05.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   bt.addEventListener('click', ()=>{
     let n = Math.floor(Math.random()* 6) + 1; //1~6까지 생성
-    img.setAttribute('src', `../img/${n}.png`);
+    img.setAttribute('src', `./img/${n}.png`);
     img.setAttribute('alt', `${n}`);
     alert(bt.innerHTML);
 
