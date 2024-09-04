9d1f9e889a18423c6379f249c4c0b8e1f7cfd667
Junyoung_Kim <zzz5686@naver.com>
2024-09-04 14:10:47 +0900

    ddf

--- 08/08.js
+++ 08/08.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded',()=>{
 //요소 가져오기
-const img = document.querySelector('.whatimg'>img);
+const img = document.querySelector("#aa");
 const txt1 = document.querySelector("#Nu");
 const bt1 = document.querySelector("#bt1");
 
 let ramdom;
 
 bt1.addEventListener("click", (e)=>{
-  e.preventDefault
-  Math.floor(Math.random()*100) + 1; // 버튼을 누를떄 마다 1~100사이
+  e.preventDefault();
+  random = Math.floor(Math.random()*100) + 1; // 버튼을 누를떄 마다 1~100사이
   console.log('radom=', ramdom);
 
   if(ramdom === parseInt(txt1.value)) {
-    img.setAttribute('src','..');
+    img.setAttribute('src','../img/good.png');
   }
   else if (ramdom > parseInt(txt1.value)) {
+    img.setAttribute('src','../img/up.png');
   }
   else{
-    img.setAttribute('src','./')
+    img.setAttribute('src','../img/down.png')
   }
 })
 
